cc454caae45d8642b49830a760f20430f81f770e
James Marriott <marriottjm5@gmail.com>
2026-08-02 02:11:09 -0700

    Fix discount factor: divide instead of multiply

--- model.py
+++ model.py
@@ -1,2 +1,2 @@
 def present_value(cash_flow, rate, year):
-    return cash_flow * (1 + rate) ** year   # <-- bug: should be division
+    return cash_flow / (1 + rate) ** year   # discount: divide to bring future cash to present value
